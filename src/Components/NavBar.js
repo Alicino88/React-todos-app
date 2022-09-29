3e0b2f869a0abd7e468af548e7a2f5a8f88da753
Alice Moretti <ali.moret88@gmail.com>
2022-09-29 14:31:43 +0300

    Changed Link with LinkContainer

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,19 +2,26 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 
+import { LinkContainer } from "react-router-bootstrap";
+
 function NavBar(props) {
   return (
-    <Navbar bg="primary" variant="primary" className="text-white">
+    <Navbar bg="primary" variant="white">
       <Container>
-        <Navbar.Brand href="#home" className="text-white">
-          Todos
-        </Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand className="text-white" to="/">
+            Todos
+          </Navbar.Brand>
+        </LinkContainer>
         {!props.isLogged && (
-          <Nav.Link onClick={props.onSetIsLogged}>Login</Nav.Link>
+          <LinkContainer to="/login" className="text-white">
+            <Nav.Link onClick={props.onSetIsLogged}>Login</Nav.Link>
+          </LinkContainer>
         )}
-
         {props.isLogged && (
-          <Nav.Link onClick={props.onSetIsLogged}>Logout</Nav.Link>
+          <LinkContainer to="/login" className="text-white">
+            <Nav.Link onClick={props.onSetIsLogged}>Logout</Nav.Link>
+          </LinkContainer>
         )}
       </Container>
     </Navbar>
