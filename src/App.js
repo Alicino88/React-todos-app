7e3789db47f9851c65cff61fcf779bd36db8354a
Alice Moretti <ali.moret88@gmail.com>
2022-09-29 11:12:45 +0300

    Added state logic to login and logout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.scss";
 import "./custom.scss";
+import { useState } from "react";
 import NavBar from "./Components/NavBar";
 import Welcome from "./Components/Welcome";
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  const toggleLogin = () => {
+    setIsLogged(!isLogged);
+  };
   return (
     <div className="App">
-      <NavBar />
-      <Welcome />
+      <NavBar onSetIsLogged={toggleLogin} isLogged={isLogged} />
+      {!isLogged && <Welcome />}
     </div>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,12 +2,18 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 
-function NavBar() {
+function NavBar(props) {
   return (
     <Navbar bg="red" variant="red">
       <Container>
         <Navbar.Brand href="#home">Todos</Navbar.Brand>
-        <Nav.Link>Login</Nav.Link>
+        {!props.isLogged && (
+          <Nav.Link onClick={props.onSetIsLogged}>Login</Nav.Link>
+        )}
+
+        {props.isLogged && (
+          <Nav.Link onClick={props.onSetIsLogged}>Logout</Nav.Link>
+        )}
       </Container>
     </Navbar>
   );
