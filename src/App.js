3ebd053db40619f2a005a286ae69ba2d7198e2d4
Alice Moretti <ali.moret88@gmail.com>
2022-09-29 11:35:10 +0300

    Added LoginForm.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./custom.scss";
 import { useState } from "react";
 import NavBar from "./Components/NavBar";
 import Welcome from "./Components/Welcome";
+import LoginForm from "./Components/LoginForm";
 
 function App() {
   const [isLogged, setIsLogged] = useState(false);
@@ -14,6 +15,7 @@ function App() {
     <div className="App">
       <NavBar onSetIsLogged={toggleLogin} isLogged={isLogged} />
       {!isLogged && <Welcome />}
+      {isLogged && <LoginForm />}
     </div>
   );
 }
