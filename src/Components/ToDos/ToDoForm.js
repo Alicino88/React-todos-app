df9550769ae3cde7221362b9444de555f66f7423
Alice Moretti <ali.moret88@gmail.com>
2022-09-30 10:56:59 +0300

    Added useRef and made inputs available in parent component ToDos

--- src/Components/ToDos/ToDoForm.js
+++ src/Components/ToDos/ToDoForm.js
@@ -1,7 +1,26 @@
 import { Container, Form, Col, Button } from "react-bootstrap";
 import { useRef } from "react";
 
-function ToDos() {
+function ToDos(props) {
+  const titleRef = useRef();
+  const dueDateRef = useRef();
+  const categoryRef = useRef();
+
+  console.log(titleRef);
+
+  const goalSubmitHandler = (e) => {
+    e.preventDefault();
+    const enteredTitle = titleRef.current.value;
+    const dueDate = dueDateRef.current.value;
+    const enteredCathegory = categoryRef.current.value;
+    //console.log(enteredTitle);
+    // console.log(dueDate);
+    //console.log(enteredCathegory);
+    props.onAddToDos(enteredTitle, dueDate, enteredCathegory);
+    titleRef.current.value = "";
+    dueDateRef.current.value = "";
+    categoryRef.current.value = "";
+  };
   return (
     <Container>
       <Col md={5} className="mx-auto">
@@ -9,22 +28,30 @@ function ToDos() {
           <h2>Add a task</h2>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text" placeholder="Enter title" />
+            <Form.Control
+              type="text"
+              placeholder="Enter title"
+              ref={titleRef}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Due date</Form.Label>
-            <Form.Control type="date" placeholder="Enter title" />
+            <Form.Control
+              type="date"
+              placeholder="Enter title"
+              ref={dueDateRef}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Category</Form.Label>
-            <Form.Select placeholder="select category">
+            <Form.Select placeholder="select category" ref={categoryRef}>
               <option>Hobby</option>
               <option>Family</option>
               <option>Work</option>
               <option>Study</option>
             </Form.Select>
           </Form.Group>
-          <Button>Add</Button>
+          <Button onClick={goalSubmitHandler}>Add</Button>
         </Form>
       </Col>
     </Container>
