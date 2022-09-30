766559c7c2fc1261251ac62e761883a34bdc5956
Alice Moretti <ali.moret88@gmail.com>
2022-09-30 10:57:27 +0300

    Added useState and set todos

--- src/Components/ToDos/ToDos.js
+++ src/Components/ToDos/ToDos.js
@@ -1,9 +1,21 @@
 import ToDoForm from "./ToDoForm";
+import { useState } from "react";
 
 function ToDos() {
+  const [toDos, setToDos] = useState([]);
+  console.log(toDos);
+  const addToDo = (newTitle, newDate, newCathegory) => {
+    console.log(newTitle);
+    setToDos((previousToDos) => {
+      return [
+        ...previousToDos,
+        { title: newTitle, date: newDate, newCathegory },
+      ];
+    });
+  };
   return (
     <>
-      <ToDoForm />
+      <ToDoForm onAddToDos={addToDo} />
     </>
   );
 }
