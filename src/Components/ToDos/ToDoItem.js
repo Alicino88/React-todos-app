8369181d222feffd8b404a7feaa1cddd9982dadc
Alice Moretti <ali.moret88@gmail.com>
2022-10-03 13:51:26 +0300

    Styled ToDoItem

--- src/Components/ToDos/ToDoItem.js
+++ src/Components/ToDos/ToDoItem.js
@@ -2,16 +2,20 @@ import { ListGroupItem, Badge } from "react-bootstrap";
 
 function toDoItem(props) {
   console.log(props.date);
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
+  //const year = props.date.getFullYear();
   return (
     <ListGroupItem
       as="li"
-      className="d-flex justify-content-between align-items-start"
       key={props.title}
+      className="d-flex justify-content-between"
     >
-      {props.title}
+      <div className="ms-2 me-auto"> {props.title}</div>
       <Badge pill bg="warning" text="dark">
-        {props.date}
+        <span>{day}</span> <span>{month}</span> <span>{year}</span>
       </Badge>
     </ListGroupItem>
   );
