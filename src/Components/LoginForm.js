f781054e1124257c7bea4953b7c54240a6a9ce9a
Alice Moretti <ali.moret88@gmail.com>
2022-10-05 21:55:20 +0300

    Added LoginForm validation

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -2,10 +2,47 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Container, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Login(props) {
+  const [emailIsValid, setEmailIsValid] = useState(true);
+  const [passwordIsValid, setPasswordIsValid] = useState(true);
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const emailChangeHandler = (e) => {
+    const email = e.target.value;
+    console.log(email);
+    if (email.includes("@")) {
+      setEmailIsValid(true);
+    } else {
+      setEmailIsValid(false);
+    }
+    setEmailValue(email);
+  };
+
+  const passwordChangeHandler = (e) => {
+    const password = e.target.value;
+    if (password.trim().length >= 5) {
+      setPasswordIsValid(true);
+    } else {
+      setPasswordIsValid(false);
+    }
+    setPasswordValue(password);
+  };
+
+  useEffect(() => {
+    if (emailIsValid && passwordIsValid) {
+      setFormIsValid(true);
+    } else if (!emailIsValid || !passwordIsValid) {
+      setFormIsValid(false);
+    }
+  }, [emailValue, passwordValue, emailIsValid, passwordIsValid]);
   const submit = (e) => {
     e.preventDefault();
+
     props.onLogin();
   };
   return (
@@ -14,7 +51,12 @@ function Login(props) {
         <Form className="mt-5">
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={emailChangeHandler}
+              className={!emailIsValid ? "border border-danger" : ""}
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
@@ -22,10 +64,20 @@ function Login(props) {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={passwordChangeHandler}
+              className={!passwordIsValid ? "border border-danger" : ""}
+            />
           </Form.Group>
 
-          <Button onClick={submit} variant="primary" type="submit">
+          <Button
+            onClick={submit}
+            variant="primary"
+            type="submit"
+            disabled={!formIsValid}
+          >
             <Link to="/mytodos" className="text-white text-decoration-none">
               Login
             </Link>
