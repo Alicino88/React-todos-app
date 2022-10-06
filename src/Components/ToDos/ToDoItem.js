5b8d5b4fa94fe05d0358128ca05f4a16561a13f1
Alice Moretti <ali.moret88@gmail.com>
2022-10-06 11:26:11 +0300

    Styled ToDoItem

--- src/Components/ToDos/ToDoItem.js
+++ src/Components/ToDos/ToDoItem.js
@@ -1,4 +1,5 @@
 import { ListGroupItem, Badge } from "react-bootstrap";
+import deleteIcon from "../../Assets/delete-icon.png";
 
 function toDoItem(props) {
   //console.log(props.date);
@@ -15,16 +16,23 @@ function toDoItem(props) {
     <ListGroupItem
       as="li"
       key={props.title}
-      className="d-flex justify-content-between"
-      onClick={removeToDo}
+      className="d-flex justify-content-between align-items-center fw-bold"
     >
-      <div className="ms-2 me-auto">
-        {" "}
-        {props.title} {props.category}
-      </div>
-      <Badge pill bg="warning" text="dark">
-        <span>{day}</span> <span>{month}</span> <span>{year}</span>{" "}
+      <div className="ms-2 me-auto fw-normal"> {props.title}</div>
+
+      <Badge pill bg="warning" text="dark" className="me-3 p-2">
+        <span>{day}</span> <span>{month}</span> <span>{year}</span>
       </Badge>
+      <Badge pill className="fw-normal p-2">
+        {props.category}
+      </Badge>
+      <img
+        src={deleteIcon}
+        alt="Delete icon by Icons8"
+        height={30}
+        className="ms-3 cursor-pointer"
+        onClick={removeToDo}
+      />
     </ListGroupItem>
   );
 }
